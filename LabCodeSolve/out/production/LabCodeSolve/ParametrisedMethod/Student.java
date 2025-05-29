67e5a34fcfc9cba4b6ac276be510e0762c651b5b
Shishir mondal <128738233+shishirmondal13@users.noreply.github.com>
2025-05-29 23:28:48 +0600

    Add files via upload

---
+++ LabCodeSolve/out/production/LabCodeSolve/ParametrisedMethod/Student.java
@@ -0,0 +1,23 @@
+package ParametrisedMethod;
+
+public class Student {
+
+    String Name,Gender,City;
+    int Age;
+
+    void setInformation(String Na,String Ge,String Ci,int Ag)
+    {
+        Name = Na;
+        Gender = Ge;
+        City = Ci;
+        Age = Ag;
+    }
+    void DisplayFullData()
+    {
+        System.out.println("Name is:"+Name);
+        System.out.println("Gender is:"+Gender);
+        System.out.println("City is: "+City);
+        System.out.println("Age is:"+Age);
+
+    }
+}
---
+++ LabCodeSolve/out/production/LabCodeSolve/ParametrisedMethod/Test.java
@@ -0,0 +1,18 @@
+package ParametrisedMethod;
+
+public class Test {
+
+    public static void main(String[]args)
+    {
+        Student S1 = new Student();
+        S1.setInformation("Karim","Male","Dhaka",25);
+        S1.DisplayFullData();
+
+        Student S2 = new Student();
+        S2.setInformation("Karina","Female","Barishal",35);
+        S2.DisplayFullData();
+
+    }
+
+
+}
